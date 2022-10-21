d80ce5df9de6cf3536298108b8617867c6e73f48
Alex Cardstack <93225030+alex-cardstack@users.noreply.github.com>
2022-10-21 15:24:26 +0100

    Fixup npm package

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -2,7 +2,7 @@
 
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import "hardhat/types/config";
-import "hardhat/types/runtime";
+// import "hardhat/types/runtime";
 
 declare module "hardhat/types/config" {
   // This is an example of an extension to one of the Hardhat config values.
