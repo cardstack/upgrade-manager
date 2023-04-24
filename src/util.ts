b3dcfdd2e7cc4a7b0cf8feabc8a097f2bb200064
tintinthong <justinthong93@gmail.com>
2023-04-24 17:07:12 +0800

    change import

--- src/util.ts
+++ src/util.ts
@@ -3,13 +3,13 @@ import { dirname, resolve } from "path";
 
 import { VoidSigner } from "@ethersproject/abstract-signer";
 import { BaseProvider, JsonRpcProvider } from "@ethersproject/providers";
-import {
-  getProxyFactoryDeployment,
-  getSafeSingletonDeployment,
-} from "@gnosis.pm/safe-deployments";
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { getTransparentUpgradeableProxyFactory } from "@openzeppelin/hardhat-upgrades/dist/utils";
 import { hashBytecodeWithoutMetadata } from "@openzeppelin/upgrades-core";
+import {
+  getProxyFactoryDeployment,
+  getSafeSingletonDeployment,
+} from "@safe-global/safe-deployments";
 import colors from "colors/safe";
 import { prompt } from "enquirer";
 import { BigNumber, Contract, ContractFactory } from "ethers";
